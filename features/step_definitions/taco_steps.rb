e140a4fc4dcd51a207fea342355b28d966894c0c
Julian Churchill <chewielouie03@gmail.com>
2012-12-18 20:35:40 +0000

    Fleshed out the basic scenarios with pending step definitions

---
+++ features/step_definitions/taco_steps.rb
@@ -0,0 +1,20 @@
+Given /^a terminal prompt$/ do
+    pending # express the regexp above with the code you wish you had
+end
+
+When /^I run `taco`$/ do
+    pending # express the regexp above with the code you wish you had
+end
+
+When /^I enter `(.*?)`$/ do |command|
+    pending # express the regexp above with the code you wish you had
+end
+
+Then /^the program should exit successfully$/ do
+    pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should see "(.*?)"$/ do |arg1|
+    pending # express the regexp above with the code you wish you had
+end
+
