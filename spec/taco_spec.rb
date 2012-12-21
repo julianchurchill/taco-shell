01ed3122efd7c864a322cdbfb9c4989f10bc67ca
Julian Churchill <chewielouie03@gmail.com>
2012-12-21 22:34:50 +0000

    Added InputFaker to simplfy tests

--- spec/taco_spec.rb
+++ spec/taco_spec.rb
@@ -1,32 +1,46 @@
 require_relative '../lib/taco'
 
+class InputFaker
+  def initialize
+    @strings = [""]
+  end
+
+  def prime_with strings
+    @strings = strings
+  end
+
+  def gets
+    next_string = @strings.shift
+    next_string
+  end
+end
+
 describe "Taco" do
   it "should present a prompt for input" do
-    output = double('output')
+    output = mock('output')
     output.should_receive(:puts).with("taco> ")
-    STDIN.should_receive(:gets).and_return("")
 
-    taco = Taco.new output, STDIN
+    taco = Taco.new output, InputFaker.new
     taco.process_input
   end
 
   it "should echo a string with the echo command" do
-    output = double('output')
+    (input = InputFaker.new).prime_with [ "echo simplestring" ]
+    output = mock('output')
     output.should_receive(:puts).with("taco> ")
-    STDIN.should_receive(:gets).and_return("echo simplestring")
     output.should_receive(:puts).with("simplestring")
 
-    taco = Taco.new output, STDIN
+    taco = Taco.new output, input
     taco.process_input
   end
 
   it "should echo a blank line if no string specified" do
-    output = double('output')
+    (input = InputFaker.new).prime_with [ "echo" ]
+    output = mock('output')
     output.should_receive(:puts).with("taco> ")
-    STDIN.should_receive(:gets).and_return("echo")
     output.should_receive(:puts).with("")
 
-    taco = Taco.new output, STDIN
+    taco = Taco.new output, input
     taco.process_input
   end
 end
