dd3611f21e815dacaae44236d9b46028a734b9a6
Julian Churchill <chewielouie03@gmail.com>
2012-12-21 22:17:44 +0000

    Refactor: Extracted methods for outputting prompt, get next command and processing the command

--- lib/taco.rb
+++ lib/taco.rb
@@ -1,14 +1,27 @@
 
 class Taco
+  @@prompt = "taco> "
+
   def initialize output, input
     @output_stream = output
     @input_stream = input
   end
 
   def process_input
-    @output_stream.puts( "taco> ")
-    input = @input_stream.gets
-    echo_command_match = /echo *(.*)/.match( input )
+    output_prompt
+    process_command get_next_command
+  end
+
+  def output_prompt
+    @output_stream.puts( @@prompt )
+  end
+
+  def get_next_command
+    @input_stream.gets
+  end
+
+  def process_command command
+    echo_command_match = /echo *(.*)/.match( command )
     if echo_command_match
       @output_stream.puts( echo_command_match[1] )
     end
